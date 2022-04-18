8988b66758d480ff59ae468b18fa9dd238524623
Lucas Paula <luolcami@gmail.com>
2022-04-18 19:23:06 +0100

    version bump 0.1.1

--- microsoft_service_wrapper/__init__.py
+++ microsoft_service_wrapper/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Lucas Paula"""
 __email__ = "luolcami@gmail.com"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/lucas8107/microsoft_service_wrapper",
-    version="0.1.0",
+    version="0.1.1",
     zip_safe=False,
 )
