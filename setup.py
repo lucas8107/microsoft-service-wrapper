1d343786d1410c9ed74569ae968071030f603d7c
Lucas Paula <luolcami@gmail.com>
2022-04-18 18:59:52 +0100

    python version fixed

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     description="A python wrapper for microsoft services REST API (currently: Graph and PowerBI)",
     install_requires=requirements,
